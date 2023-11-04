bcf6fac9d74a4f2e28ccfc1d24b0d3cc2a0b53aa
Tang Yetong <tangyetong666@gmail.com>
2023-11-04 20:14:53 +0800

    run in npx expo run:android

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
+import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import useBLE from "./src/useBLE";
 
 export default function App() {
+  const {
+    requestPermissions,
+    scanForPeripherals,
+    allDevices,
+    connectedDevice,
+    heartRate,
+  } = useBLE();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    scanForDevices();
+    setIsModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app! Test!</Text>
       <StatusBar style="auto" />
     </View>
   );
