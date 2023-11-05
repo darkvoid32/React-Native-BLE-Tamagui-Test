161d608dd3db8f86d8617dd3eaa691ff1c280e3b
Yetong <tangyetong666@gmail.com>
2023-11-05 23:11:13 +0800

    Working Scanner

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+} from "react-native";
 import useBLE from "./src/useBLE";
 
 export default function App() {
@@ -29,18 +36,66 @@ export default function App() {
   };
 
   return (
+    <>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app! Test!</Text>
       <StatusBar style="auto" />
     </View>
+
+    <SafeAreaView style={styles.container}>
+    <Button
+      onPress={openModal}
+      title="Scan Devices"
+      color="#841584"
+      accessibilityLabel="Scan Devices"
+    />
+    </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f2f2f2",
+  },
+  heartRateTitleWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heartRateTitleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginHorizontal: 20,
+    color: "black",
+  },
+  heartRateText: {
+    fontSize: 25,
+    marginTop: 15,
+  },
+  ctaButton: {
+    backgroundColor: "#FF6060",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 8,
+  },
+  ctaButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
   },
 });
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
