9d56497fd7b5e57ea3f7f25b0795a6ad06bedd6a
Yetong <tangyetong666@gmail.com>
2023-11-06 17:21:43 +0800

    Playing with list

--- App.tsx
+++ App.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import { StatusBar } from 'expo-status-bar';
 import {
   SafeAreaView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
   Button,
 } from "react-native";
-import useBLE from "./src/useBLE";
+import useBLE from "./src/utils/useBLE";
+import DeviceList from "./src/components/deviceList";
 
 export default function App() {
   const {
@@ -17,6 +16,7 @@ export default function App() {
     allDevices,
     connectedDevice,
   } = useBLE();
+  
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const scanForDevices = async () => {
@@ -36,66 +36,23 @@ export default function App() {
   };
 
   return (
-    <>
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! Test!</Text>
-      <StatusBar style="auto" />
-    </View>
-
     <SafeAreaView style={styles.container}>
-    <Button
-      onPress={openModal}
-      title="Scan Devices"
-      color="#841584"
-      accessibilityLabel="Scan Devices"
-    />
+      <Button
+        onPress={openModal}
+        title="Scan Devices"
+        color="#841584"
+        accessibilityLabel="Scan Devices"
+      />
+      <DeviceList/>
     </SafeAreaView>
-    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f2f2f2",
-  },
-  heartRateTitleWrapper: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  heartRateTitleText: {
-    fontSize: 30,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginHorizontal: 20,
-    color: "black",
-  },
-  heartRateText: {
-    fontSize: 25,
-    marginTop: 15,
-  },
-  ctaButton: {
-    backgroundColor: "#FF6060",
-    justifyContent: "center",
-    alignItems: "center",
-    height: 50,
-    marginHorizontal: 20,
-    marginBottom: 5,
-    borderRadius: 8,
-  },
-  ctaButtonText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "white",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
